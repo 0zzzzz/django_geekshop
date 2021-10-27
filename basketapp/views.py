bf835d62f4df8090477e5824a166b4ccc3ba58fc
ozzzzz <you@example.com>
2021-10-27 03:25:14 +0300

    basket indication

--- basketapp/views.py
+++ basketapp/views.py
@@ -11,15 +11,11 @@ def basket(request):
 
 def add(request, pk):
     product = get_object_or_404(Product, pk=pk)
-
     basket = Basket.objects.filter(user=request.user, product=product).first()
-
     if not basket:
         basket = Basket(user=request.user, product=product)
-
     basket.quantity += 1
     basket.save()
-
     return HttpResponseRedirect(request.META.get('HTTP_REFERER'))
 
 
