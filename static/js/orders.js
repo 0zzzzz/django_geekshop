83d33aca19901e833c111169794b91dd2abd53b7
ozzzzz <you@example.com>
2021-12-02 00:28:52 +0300

    orderitem calsses

--- static/js/orders.js
+++ static/js/orders.js
@@ -18,20 +18,24 @@ window.onload = function () {
             price_arr[i] = 0;
         }
     }
-    // console.log(price_arr)
+
+
     // console.log(quantity_arr)
 
 
     $('.order_form').on('click', 'input[type=number]', function () {
         let target = event.target;
         orderitem_num = parseInt(target.name.replace('orderitems-', '').replace('-quantity', ''));
-
+        console.log(price_arr[orderitem_num])
         if (price_arr[orderitem_num]) {
             orderitem_quantity = parseInt(target.value);
             delta_quantity = orderitem_quantity - quantity_arr[orderitem_num];
+            // console.log(delta_quantity)
             quantity_arr[orderitem_num] = orderitem_quantity;
             order_summary_update(price_arr[orderitem_num], delta_quantity)
         }
+        console.log(price_arr)
+        console.log(quantity_arr)
     });
 
     $('.order_form').on('click', 'input[type=checkbox]', function () {
@@ -46,6 +50,8 @@ window.onload = function () {
     });
 
     function order_summary_update(orderitem_price, delta_quantity) {
+        // console.log(orderitem_price)
+        // console.log(delta_quantity)
         delta_cost = orderitem_price * delta_quantity;
         order_total_cost = Number((order_total_cost + delta_cost).toFixed(2));
         order_total_quantity = order_total_quantity + delta_quantity;
@@ -57,18 +63,42 @@ window.onload = function () {
 
     $('.order_form').on('click', 'select', function () {
         let target = event.target;
-        console.log(target)
         orderitem_num = parseInt(target.name.replace('orderitems-', '').replace('-product', ''));
         _price = parseFloat($('.orderitems-' + orderitem_num + '-price').text().replace(',', '.'));
         _quantity = parseInt($('input[name="orderitems-' + orderitem_num + '-quantity"]').val());
-        let test_value = target.value
-        $('.orderitems-' + orderitem_num + '-price').text(test_value.toString());
-        let order_total = Number(order_total_cost - _price * _quantity + test_value * _quantity).toFixed(2);
+        if (isNaN(_quantity)) {
+            _quantity = 0
+        }
+        let ajax_price = target.value
+        $('.orderitems-' + orderitem_num + '-price').text(ajax_price.toString());
+        let order_total = Number(order_total_cost - _price * _quantity + ajax_price * _quantity).toFixed(2);
         order_total_cost = parseFloat(order_total);
         $('.order_total_cost').html(order_total_cost.toString());
         _price = parseFloat($('.orderitems-' + orderitem_num + '-price').text().replace(',', '.'));
         price_arr[orderitem_num] = _price
-
+        // console.log(price_arr)
+        // $(document).ready(function() {
+        //     $('#id').change(function() {
+        //         let query = $(this).val();
+        //         console.log(query);
+        //         $.ajax({
+        //             url: "/update/",
+        //             type: "POST",
+        //             dataType: "json",
+        //             data: {
+        //                 client_response: query
+        //             },
+        //             success: function (json) {
+        //                 console.log(document.getElementById('id_amount').value = json.amount)
+        //                 document.getElementById('id_amount').value = json.amount;
+        //             },
+        //             failure: function (json) {
+        //                 alert('Got an error dude');
+        //             }
+        //         });
+        //     })
+        // })
+        // console.log('asdfgsdfgsdfgsdfgdsfg')
     });
 
 
@@ -84,17 +114,28 @@ window.onload = function () {
         let target_name = row[0].querySelector('input[type="number"]').name;
         orderitem_num = parseInt(target_name.replace('orderitems-', '').replace('-quantity', ''));
         delta_quantity = -quantity_arr[orderitem_num];
+        quantity_arr.splice(orderitem_num, 1)
+        console.log(orderitem_num)
+        // console.log(quantity_arr)
+        // console.log(quantity_arr[orderitem_num])
+        price_arr.splice(orderitem_num, 1)
+        console.log(price_arr[orderitem_num])
+        console.log(price_arr)
         order_summary_update(price_arr[orderitem_num], delta_quantity);
     }
 
     function add_order_item(row) {
-        orderitem_num = quantity_arr.length
+        let arr_length = quantity_arr.length
         quantity_arr.push(0)
-        price_arr.push(quantity_arr.length)
-        delta_quantity = +quantity_arr[orderitem_num];
-        order_summary_update(price_arr[orderitem_num], delta_quantity);
-        let element = document.getElementsByClassName('td3')[quantity_arr.length]
-        let elem_class = element.getElementsByTagName('span')
-        elem_class[0].className='orderitems-' + (quantity_arr.length - 1) + '-price'
+        price_arr.push(0)
+        delta_quantity = quantity_arr[arr_length];
+        let selector = document.getElementsByClassName('td3')[arr_length+1]
+        selector = selector.getElementsByTagName('span')
+        selector[0].setAttribute('class', 'orderitems-' + (arr_length) + '-price')
+        selector = document.getElementsByClassName('td2')[arr_length+1]
+        selector = selector.getElementsByTagName('input')
+        selector[0].setAttribute('name', 'orderitems-' + (arr_length) + '-quantity')
+        selector[0].setAttribute('id', 'orderitems-' + (arr_length) + '-quantity')
+        order_summary_update(0, 0);
     }
 }
